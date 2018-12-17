fcc899430ba6da01fb9a8539d7e19b07107bf723
alanpeng1204 <mxda940107@hotmail.com>
2018-12-17 16:34:31 +0800

    SmallChanges

---
+++ Assets/AutoMoving.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMoving : MonoBehaviour {
+    [Range(0, 4)]
+    public float Speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += this.transform.forward * Speed * Time.deltaTime;
+	}
+}
